0194dbfdb25ba3b55edd3256d2c7789e03a66fa9
tankejie <108395229+tankejie@users.noreply.github.com>
2022-07-14 18:56:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def predict():
     # print(txt)
     
 #     return render_template('index.html', prediction_text='The predicted abalone age is: {}'.format(pred_result))
-    return render_template('index.html', predictions=predictions)
+    return render_template('index.html', prediction_text=predictions)
 
 if __name__ == "__main__":
     app.run()
