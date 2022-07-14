2cbb6f4f7c16eb67ed8e5a7798bffe21ff9974a7
tankejie <108395229+tankejie@users.noreply.github.com>
2022-07-15 07:47:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def predict():
     #     txt = 'Risky Loan'
     # print(txt)
     return render_template('index.html', prediction_text=prediction_text)
-#     return render_template('index.html', prediction_text='The predicted abalone age is: {}.'.format(pred_result))
-#     return render_template('index.html', prediction_text=prediction_text)
+    # return render_template('index.html', prediction_text='The predicted abalone age is: {}.'.format(pred_result))
+    # return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == "__main__":
     app.run()
