f6c4fce0680537c84732c9c2856b2a807c3baa72
tankejie <108395229+tankejie@users.noreply.github.com>
2022-07-14 18:01:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ import requests
 app = Flask(__name__)
 model = pickle.load(open('model.h5','rb'))
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        length = request.form.get('length')
+        diameter = request.form.get('diameter')
+        height = request.form.get('height')
+        return redirect(url_for('predict', testdata=[[0,length,diameter,height,0,0,0,0]]))
     return render_template('index.html')
 
 
-@app.route('/predict',methods=['GET', 'POST'])
-def predict():
+@app.route('/predict')
+def predict(testdata):
     # Age = request.form.get('Rings')
     # if(Gender=='Female'):
     #     Gender=0
@@ -23,9 +28,9 @@ def predict():
     #     MStatus=0
     # else:
     #     MStatus=1
-    length = request.form.get('length')
-    diameter = request.form.get('diameter')
-    height = request.form.get('height')
+#     length = request.form.get('length')
+#     diameter = request.form.get('diameter')
+#     height = request.form.get('height')
     # LoanDuration = request.form.get('duration')              
    
     ##Test model prediction with static data. Reshape to change to 2D array 
@@ -41,7 +46,7 @@ def predict():
 #     ],(1, -1))
     
 #     model = pickle.load(open('model.h5','rb'))
-    pred_result = model.predict([[0,0.3,0.3,0.3,0,0,0,0]])
+    pred_result = model.predict(testdata)
 
 #     pred_result = model.predict(testdata)
 
