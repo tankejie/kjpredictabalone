8dc2cb11f0b5aa8c8b43196c9f0d3e8965401e31
tankejie <108395229+tankejie@users.noreply.github.com>
2022-07-14 18:08:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ import requests
 app = Flask(__name__)
 model = pickle.load(open('model.h5','rb'))
 
+# @app.route('/')
+# def home():
+#     if request.method == 'POST':
+#         length = request.form.get('length')
+#         diameter = request.form.get('diameter')
+#         height = request.form.get('height')
+#         return redirect(url_for('predict', testdata=[[0,length,diameter,height,0,0,0,0]]))
+#     return render_template('index.html')
+
+
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        length = request.form.get('length')
-        diameter = request.form.get('diameter')
-        height = request.form.get('height')
-        return redirect(url_for('predict', testdata=[[0,length,diameter,height,0,0,0,0]]))
-    return render_template('index.html')
-
-
-@app.route('/predict/<testdata>')
-def predict(testdata):
+def predict():
     # Age = request.form.get('Rings')
     # if(Gender=='Female'):
     #     Gender=0
@@ -28,27 +28,27 @@ def predict(testdata):
     #     MStatus=0
     # else:
     #     MStatus=1
-#     length = request.form.get('length')
-#     diameter = request.form.get('diameter')
-#     height = request.form.get('height')
+    length = request.form.get('length')
+    diameter = request.form.get('diameter')
+    height = request.form.get('height')
     # LoanDuration = request.form.get('duration')              
    
     ##Test model prediction with static data. Reshape to change to 2D array 
-#     testdata = np.reshape([
-#     0,
-#     length,
-#     diameter,
-#     height,
-#     0,
-#     0,
-#     0,
-#     0
-#     ],(1, -1))
+    testdata = np.reshape([
+    0,
+    length,
+    diameter,
+    height,
+    0,
+    0,
+    0,
+    0
+    ],(1, -1))
     
 #     model = pickle.load(open('model.h5','rb'))
-    pred_result = model.predict(testdata)
-
 #     pred_result = model.predict(testdata)
+
+    pred_result = model.predict(testdata)
 
     # txt = "The predicted abalone age is {}.".format(pred_result)
 
